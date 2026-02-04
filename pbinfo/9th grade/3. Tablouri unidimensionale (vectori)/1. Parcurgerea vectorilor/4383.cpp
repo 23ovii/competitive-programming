24f01f20ff21602a145c883f256df032c4b1a826
23ovii <danyovidiu21@gmail.com>
2026-02-04 19:34:51 +0200

    solved

---
+++ pbinfo/9th grade/3. Tablouri unidimensionale (vectori)/1. Parcurgerea vectorilor/4383.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool prim(int n) {
+    if (n < 2) return false;
+    if (n % 2 == 0 && n > 2) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, a[1001];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = n - 1; i > 0; i--) {
+        if (prim(a[i])) {
+            a[i] = 0;
+            break;
+        }
+    }
+    for(int i = 0; i < n; i++) cout << a[i] << " ";
+    return 0;
+}
