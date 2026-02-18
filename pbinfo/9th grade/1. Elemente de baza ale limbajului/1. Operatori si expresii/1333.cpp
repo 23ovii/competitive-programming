a840473bfff29c4bbb1bd83caedf23087d49b1a5
23ovii <danyovidiu21@gmail.com>
2026-02-18 20:28:36 +0200

    solved

---
+++ pbinfo/9th grade/1. Elemente de baza ale limbajului/1. Operatori si expresii/1333.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long double B, b, l;
+    cin>>B>>b>>l;
+    long double h = sqrt ( pow(l,2) - pow((B-b)/2,2) );
+    long double bt = b + (B-b)/2;
+    long double d = sqrt ( pow(h,2) + pow(bt,2));
+    cout << d << "\n";
+    return 0;
+}
