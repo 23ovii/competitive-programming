41a6fc7ca6efdfaacb2f79daa7e4481a2d9517b6
23ovii <danyovidiu21@gmail.com>
2026-02-18 20:06:47 +0200

    solved

---
+++ pbinfo/9th grade/1. Elemente de baza ale limbajului/1. Operatori si expresii/3070.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+    cout << (2 * a + 2  * b) << " " << a * b << " " << a * a + b * b << "\n";
+    return 0;
+}
