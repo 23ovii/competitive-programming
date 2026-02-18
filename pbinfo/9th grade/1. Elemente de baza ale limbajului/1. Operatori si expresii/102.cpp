8e3e488b68ca6c9a1dcc3395af76577c5af0c14f
23ovii <danyovidiu21@gmail.com>
2026-02-18 19:26:39 +0200

    solved

--- pbinfo/9th grade/1. Elemente de baza ale limbajului/1. Operatori si expresii/102.cpp
+++ pbinfo/9th grade/1. Elemente de baza ale limbajului/1. Operatori si expresii/102.cpp
@@ -5,8 +5,8 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long n, m;
-    cin >> n >> m;
-    cout << n;
+    int n;
+    cin >> n;
+    cout << n % 10 + n / 10 % 10 << "\n";
     return 0;
 }
