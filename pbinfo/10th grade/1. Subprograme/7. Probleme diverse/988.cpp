0e72e025f737bcfd2d95f69866abdae190412942
23ovii <danyovidiu21@gmail.com>
2026-04-03 18:24:24 +0300

    solved

--- pbinfo/10th grade/1. Subprograme/7. Probleme diverse/988.cpp
+++ pbinfo/10th grade/1. Subprograme/7. Probleme diverse/988.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int prime(int n) {
     if (n < 2 ) return 0;
     if (n % 2 == 0 && n > 2) return 0;
-    for (int i = 3; i <= i * i; i += 2) {
+    for (int i = 3; i * i <= n; i += 2) {
         if (n % i == 0) {
             return 0;
         }
