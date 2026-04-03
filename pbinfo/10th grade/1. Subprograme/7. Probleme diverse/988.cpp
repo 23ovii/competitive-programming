fa531544e63a8e1d939564caf80c45bc44ec0438
23ovii <danyovidiu21@gmail.com>
2026-04-03 18:21:29 +0300

    solved

---
+++ pbinfo/10th grade/1. Subprograme/7. Probleme diverse/988.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int prime(int n) {
+    if (n < 2 ) return 0;
+    if (n % 2 == 0 && n > 2) return 0;
+    for (int i = 3; i <= i * i; i += 2) {
+        if (n % i == 0) {
+            return 0;
+        }
+    } return 1;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ifstream fin("prime.in");
+    ofstream fout("prime.out");
+
+    int n, x;
+    fin >> n;
+    for (int i = 0 ; i < n; i++) {
+        fin >> x;
+        if (prime(x)) {
+            fout << x << " ";
+        }
+    }
+    return 0;
+}
