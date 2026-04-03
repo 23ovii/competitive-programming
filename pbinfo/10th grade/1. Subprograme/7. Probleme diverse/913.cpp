4d3c76eef367371e7226c3f3b62849ff63b715db
23ovii <danyovidiu21@gmail.com>
2026-04-03 18:37:37 +0300

    solved

---
+++ pbinfo/10th grade/1. Subprograme/7. Probleme diverse/913.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+void citire(int &n, int a[101]) {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+}
+void afisare(int n, int a[101]) {
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
+bool prim(int n) {
+    if (n < 2) return 0;
+    if (n % 2 == 0 && n > 2) return 0;
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return 0;
+        }
+    } return 1;
+}
+int urmatorul_prim(int n) {
+    while (n++) {
+        if (prim(n)) {
+            return n;
+        }
+    }
+}
+void inloc(int n, int a[]) {
+    for (int i = 0; i < n; i++) {
+        if (!prim(a[i])) {
+            a[i] = urmatorul_prim(a[i]);
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, a[101];
+    citire(n, a);
+    inloc(n, a);
+    afisare(n, a);
+    return 0;
+}
