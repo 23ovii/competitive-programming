61410f44d30ad782e5916d7bb858c4aa534a0519
23ovii <danyovidiu21@gmail.com>
2026-04-24 20:00:10 +0300

    solved

--- pbinfo/9th grade/1. Elemente de baza ale limbajului/2. Structura de decizie/4897.cpp
+++ pbinfo/9th grade/1. Elemente de baza ale limbajului/2. Structura de decizie/4897.cpp
@@ -7,13 +7,21 @@ int main() {
 
     int n;
     cin >> n;
-    int inverse = 0;
-    while (n > 0) {
-        int digit = n % 10;
-        inverse = inverse * 10 + digit;
-        n /= 10;
+    int temp = n;
+    int nr = 0;
+    int inv = 0;
+    while (temp) {
+        nr++;
+        inv = inv * 10 + temp % 10;
+        temp /= 10;
     }
-    if (inverse == n) {
+    if (nr == 1) {
+        cout << "DA";
+    } else if (nr == 2 && n % 11 == 0) {
+        cout << "DA";
+    } else if (nr == 3 && n % 10 == n / 100) {
+        cout << "DA";
+    } else if (nr == 4 && inv ==  n) {
         cout << "DA";
     } else {
         cout << "NU";
