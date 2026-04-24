e07b0eebeeff7e4f2568e969908f622af6c4eac1
23ovii <danyovidiu21@gmail.com>
2026-04-24 19:18:47 +0300

    solved

---
+++ pbinfo/9th grade/1. Elemente de baza ale limbajului/2. Structura de decizie/109.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "n este par\n";
+    } else {
+        cout << "n este impar\n";
+    }
+    return 0;
+}
